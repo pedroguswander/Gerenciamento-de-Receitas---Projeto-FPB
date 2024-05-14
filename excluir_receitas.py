ec2359a8ee0c1b7fb1fd8bfec2d4ebe7b8f33c44
Pedro de Araújo <padg@cesar.school>
2024-05-14 12:21:40 -0300

    conflito resolvido

--- excluir_receitas.py
+++ excluir_receitas.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 def excluirReceitas() :
     while(True) :
         file = open("CRUD_projeto_python_1_unidade/receitas.txt","r",encoding="utf8")
@@ -28,34 +27,3 @@ def registroSem_a_excluida(lista_de_receitas) :
         file.write(f"{receita}\n")
         
 
-=======
-def excluirReceitas() :
-    while(True) :
-        file = open("CRUD_projeto_python_1_unidade/receitas.txt","r",encoding="utf8")
-        lista_de_receitas = file.readlines()
-        receita_excluida = input("Qual a receita que deseja excluir?\n")
-        file.close()
-        file = open("CRUD_projeto_python_1_unidade/receitas.txt","w",encoding="utf8")
-        file.write("")
-        file.close()
-        for i in range(len(lista_de_receitas)) :
-            if receita_excluida in lista_de_receitas[i] :
-                lista_de_receitas.pop(i)
-                break
-            elif i == len(lista_de_receitas)-1 :
-                print("Não há como excluir a receita pois não há o seu registro no sistema")
-                registroSem_a_excluida(lista_de_receitas)
-        registroSem_a_excluida(lista_de_receitas)
-        resposta = input("Deseja continuar excluindo receitas?\n").capitalize()
-        if resposta == 'Não' or resposta == 'N' :
-            break
-        else : 
-            pass
-
-def registroSem_a_excluida(lista_de_receitas) :
-    file = open("CRUD_projeto_python_1_unidade/receitas.txt","w",encoding="utf8")
-    for receita in lista_de_receitas :
-        file.write(f"{receita}\n")
-        
-
->>>>>>> 95d924e2a136dc816690e51f6801334d74651a35
--- visualizar_receitas.py
+++ visualizar_receitas.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import os
 os.system("cls")
 def visualizarReceitas() :
@@ -18,25 +17,4 @@ def visualizarReceitas() :
             break
         else : 
             pass
-=======
-import os
-os.system("cls")
-def visualizarReceitas() :
-    while(True) :
-        file = open("CRUD_projeto_python_1_unidade/receitas.txt","r",encoding="utf8")
-        lista_receitas = file.readlines()
-        receita_visualizada = input("Qual receita deseja visualizar?\n")
-        for i in range(len(lista_receitas)) :
-            if receita_visualizada in lista_receitas[i] :
-                print(lista_receitas[i])
-                break
-            if  i == len(lista_receitas)-1 :
-                print("Essa receita não foi registrada ainda")
-                # deseja registrar essa receita?
-        resposta = input("deseja continuar vendo receitas?\n").capitalize()
-        if resposta == 'Não' or resposta == 'N' :
-            break
-        else : 
-            pass
->>>>>>> 95d924e2a136dc816690e51f6801334d74651a35
     
