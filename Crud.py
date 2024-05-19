b8b8ee19f8d42f090197fb8e13743ba081190b7b
Pedro de Araújo <padg@cesar.school>
2024-05-19 11:57:35 -0300

    adicionando receitas por país de origem

--- Crud.py
+++ Crud.py
@@ -1,25 +1,45 @@
 import os
-os.system("cls")
+os.system('cls')
 import registrar_receitas
 import visualizar_receitas
 import excluir_receitas
 import favoritar_receitas
+import receitas_aleatorias
 import atualizar_receitas
-print("Bem vindo ao CRUD de receitas!!")
+import receitas_por_país_de_origem
 
-print("""As opções de menu são : registrar receitas , visualizar receitas , atualizar receitas ,
-excluir receitas , favoritar receitas , visualizar as receitas de acordo com o país de origem e (opção extra)  """)
+print("Bem vindo ao CRUD de receitas!!")
+print('\033[34m=\033[m' * 50)
+print('                  \033[1;34;40mFATIADOS\033[m')
+print('\033[34m=\033[m' * 50)
+print('1 -\033[32m Registrar Receitas\033[m')
+print('2 -\033[32m Visualizar Receitas\033[m')
+print('3 -\033[32m Excluir Receitas\033[m')
+print('4 -\033[32m Favoritar Receitas\033[m')
+print('5 -\033[32m Sugestão de Receita Aleatória\033[m')
+print('6 -\033[32m Atualizar Receitas\033[m')
+print('7 -\033[32m Ver Receitas por País de Origem\033[m')
+
+print('\033[34m=\033[m' * 50)
+
+
+# print("""As opções de menu são : registrar receitas , visualizar receitas , atualizar receitas ,
+# excluir receitas , favoritar receitas , visualizar as receitas de acordo com o país de origem e (opção extra)  """)
 
 while (True) :
     print("Escolha uma opção do menu :",end=" ")
-    menu = input()
-    if menu == 'registrar receitas' :
+    menu = int(input())
+    if menu == 1 :
         registrar_receitas.registrarRecitas(registrar_receitas.receitas)
-    elif menu == 'visualizar receitas' :
+    elif menu == 2 :
         visualizar_receitas.visualizarReceitas()
-    elif menu == 'excluir receitas' :
+    elif menu == 3 :
         excluir_receitas.excluirReceitas()
-    elif menu == 'favoritar receitas' :
+    elif menu == 4 :
         favoritar_receitas.favoritar_receitas()
-    elif  menu == 'atualizar receitas' :
+    elif menu == 5 :
+        receitas_aleatorias.receitas_aleatorias()
+    elif menu == 6 :
         atualizar_receitas.atualizar_receitas()
+    elif menu == 7 :
+        receitas_por_país_de_origem.receitas_por_paisdeOrigem()
---
+++ receitas_por_país_de_origem.py
@@ -0,0 +1,24 @@
+
+def receitas_por_paisdeOrigem() :
+    file = open("receitas.txt","r",encoding="utf8")
+    lista_de_receitas = file.readlines()
+    file.close()
+    pais_e_receitas = {}
+    while(True) :
+        pais_receitas = input("Deseja ver receitas de que país?\n")
+        pais_e_receitas[pais_receitas] = []
+        for receita in lista_de_receitas :
+            if pais_receitas in receita :
+                nova_lista_de_receitas = receita.split()   
+                index_pais = nova_lista_de_receitas.index("País")
+                lista_nome_receita = []
+                for x in range(index_pais) :
+                    lista_nome_receita.append(nova_lista_de_receitas[x])
+                pais_e_receitas[pais_receitas].append(" ".join(lista_nome_receita))
+        print(pais_receitas,":")
+        for i in range(len(pais_e_receitas[pais_receitas])) :
+            print(pais_e_receitas[pais_receitas][i],end=",")
+        resposta = input("\ndeseja continuar vendo receitas por país de origem?\n").capitalize()
+        if resposta == "Não" or resposta == 'N' :
+            break
+            
