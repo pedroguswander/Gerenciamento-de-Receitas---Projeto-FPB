633f5478c5fb8eaab84fb7c89b6fa518480b5190
Felipe <fgm@cesar.school>
2024-05-19 13:59:24 -0300

    Modificando o receitas aleatorias

--- receitas_aleatorias.py
+++ receitas_aleatorias.py
@@ -2,7 +2,7 @@ import os
 os.system("cls")
 import random
 def receitas_aleatorias():
-    file = open("receitas.txt","r",encoding="utf8")
+    file = open("receitas_aleatorias.txt","r",encoding="utf8")
     linhas = file.readlines()
     receita_aleatoria = random.choice(linhas)
     print("Que tal essa receita: ",receita_aleatoria) 
